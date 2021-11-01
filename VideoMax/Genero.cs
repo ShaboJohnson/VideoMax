6a1ed6e3bcc52865d65d9b01aec415956a50be35
ShaboJohnson <shabojohnson@gmail.com>
2021-10-31 22:27:10 -0500

    cambios

--- VideoMax/Genero.cs
+++ VideoMax/Genero.cs
@@ -6,5 +6,21 @@ namespace VideoMax
 {
     class Genero
     {
+        public int GeneroId { get; }
+
+        public string Description { get; }
+
+
+        public Genero (int generoId, string description)
+        {
+            GeneroId = generoId;
+            Description = description;
+
+
+
+        }
+
+
+
     }
 }
--- VideoMax/Pelicula.cs
+++ VideoMax/Pelicula.cs
@@ -6,8 +6,24 @@ namespace VideoMax
 {
     class Pelicula
     {
+        public int PeliculaId { get; }
+
+        public int GeneroId { get; }
+
+        public string Title { get; }
+
+        public int Year { get;  }
+
+        public Pelicula(int peliculaId, int generoId, string title, int year)
+
+        {
+            PeliculaId = peliculaId;
+            GeneroId = generoId;
+            Title = title;
+            Year = year;
 
 
+        }
 
     }
 }
